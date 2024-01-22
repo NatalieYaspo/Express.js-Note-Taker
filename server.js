6310f64d007fb31edad76dc984c130c223148a2b
Traspo <nyaspo8@gmail.com>
2024-01-21 20:04:30 -0500

    it sees the delete request but isn't deleting

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const notes = require('./db/db.json');
-// const bodyParser = require('body-parser');
+// const api = require('./routes/notes.js');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('./helpers/fsUtils');
 
 const PORT = 3001;
 
@@ -11,21 +16,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
 //GET request to homepage for app
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 // GET request for Notes.html
 app.get('/notes', (req, res) => {
-    // Sends to notes.html when Get Started is clicked.
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+  // Sends to notes.html when Get Started is clicked.
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
 app.get('/api/notes', (req, res) => {
@@ -34,56 +37,50 @@ app.get('/api/notes', (req, res) => {
 
 // POST request to add a Note
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a Note`);//WORKS
-  
-    // Destructuring assignment for the items in req.body
-    const { title,  text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the Note object we will save
-      const newNote = {
-        title,
-        text,
-        noteId: uuid(),
-      };
-  
-      // Obtain existing Notes
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new Note
-          parsedNotes.push(newNote);
-  
-          // Write updated Notes back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated Notes!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting Note');
-    }
-  });
-  
-  app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  );  
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a Note`);//WORKS
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the Note object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    // Obtain existing Notes
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.errored('Error in adding note');
+  }
+});
+
+//DELETE Route for a specific note
+app.delete('/api/notes/:noteID', (req, res) => {
+  // Log that a DELETE request was received
+  console.info(`${req.method} request received to delete a Note`);//It sees this.
+  const noteID = req.params.note_id;
+  console.log(req.params.note_id); //Undefined
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      //Make a new array of all the notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteID)
+      console.log(result);
+
+      //Saves the array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      //Logs which note has been deleted
+      res.json(`Item ${noteID} has been deleted.`)
+    })
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);  
